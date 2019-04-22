bd2f39910d3aad7fa7972a17fb4c5f54a969226a
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-04-22 09:42:11 -0300

    draws n squares centered on the page

--- bingo.py
+++ bingo.py
@@ -12,13 +12,28 @@ terms = ['Cidadão',
     'Estatuto da Criança e Adolescente'
     ]
     
-n_cards = 10
-size(595,842/2)
+size(595,842/2)    
+
 
 columns = math.ceil(math.sqrt(len(terms)))
 # for the middle cell to be a “free space” there needs to be at least one word
 # less than cells! (and this only works when there are an odd number of columns)
 free_spaces = columns * columns - len(terms)
+
+margin = 50
+if (width() < height()):
+    small = width()
+else:
+    small = height()
+    
+s_w = s_h = (small - margin * 2) / columns
+margin_x = (width() - s_w * columns) / 2
+margin_y = (height() - s_h * columns) / 2
+
+n_cards = 1
+
+fill(None)
+stroke(0)
 
 for card in range(n_cards):
     aux_terms = terms.copy()
@@ -27,11 +42,12 @@ for card in range(n_cards):
         aux_terms.insert(math.floor(len(terms)/2), 'Espaço Livre')
     for c in range(columns):
         for l in range(columns):
+            x = s_w * c + margin_x
+            y = s_h * l + margin_y
+            rect(x, y, s_w, s_h)
             i = c * columns + l
             if (c < len(terms)):
                 text = str(i) + ' ' + aux_terms[c * columns + l]
             else:
                 text = "Termo faltante"
-            print(text)
-    print('-----')
         

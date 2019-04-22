42de0a86f44c113a7837a89c1ab06cee11dfaaab
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-04-22 09:33:53 -0300

    shuffles list of terms for each bingo card

---
+++ bingo.py
@@ -0,0 +1,37 @@
+import math 
+import random
+
+terms = ['Cidadão', 
+    'Direito Político', 
+    'Direito social', 
+    'Direito civil', 
+    'Direitos individuais', 
+    'Direito coletivo', 
+    'Constituição', 
+    'Constituição cidadã',
+    'Estatuto da Criança e Adolescente'
+    ]
+    
+n_cards = 1
+size(595,842/2)
+
+columns = math.ceil(math.sqrt(len(terms)))
+# for the middle cell to be a “free space” there needs to be at least one word
+# less than cells! (and this only works when there are an odd number of columns)
+free_spaces = columns * columns - len(terms)
+
+for card in range(n_cards):
+    aux_terms = terms.copy()
+    random.shuffle(aux_terms)
+    if columns % 2 != 0 and free_spaces > 0:
+        terms.insert(math.floor(len(terms)/2), 'Espaço Livre')
+    for c in range(columns):
+        for l in range(columns):
+            i = c * columns + l
+            if (c < len(terms)):
+                text = terms[c * columns + l]
+            else:
+                text = "Termo faltante"
+            print(text)
+    print('-----')
+        

fc0eca38239b8632971a44c063b2fc0bb78c30a4
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-04-22 10:02:34 -0300

    draws text in each cell (finding an appropriate font size)

--- bingo.py
+++ bingo.py
@@ -31,9 +31,8 @@ margin_x = (width() - s_w * columns) / 2
 margin_y = (height() - s_h * columns) / 2
 
 n_cards = 1
+cell_margin = s_w * 0.10
 
-fill(None)
-stroke(0)
 
 for card in range(n_cards):
     aux_terms = terms.copy()
@@ -44,10 +43,35 @@ for card in range(n_cards):
         for l in range(columns):
             x = s_w * c + margin_x
             y = s_h * l + margin_y
+            
+            fill(None)
+            stroke(0)
             rect(x, y, s_w, s_h)
+            
             i = c * columns + l
             if (c < len(terms)):
-                text = str(i) + ' ' + aux_terms[c * columns + l]
+                current_term = aux_terms[c * columns + l]
             else:
-                text = "Termo faltante"
+                current_term = "Termo faltante"
+            can_print = False
+            fs = 14
+            tx = x + cell_margin
+            ty = y + cell_margin
+            tw = s_w - cell_margin * 2
+            th = s_h - cell_margin * 2
+            
+            while can_print == False and fs > 0:
+                font('Helvetica', fs)
+                overflow = textOverflow(current_term,(tx, ty, tw, th),align="center")
+                if overflow == "":
+                    can_print = True
+                else:
+                    fs = fs - 1
+            
+            stroke(None)
+            fill(0)
+            text_width, text_height = textSize(current_term, align="center", width=tw)
+            offset_y = (th - text_height) / 2
+            textBox(current_term, (tx, ty - offset_y, tw, th), align="center")
+                    
         
